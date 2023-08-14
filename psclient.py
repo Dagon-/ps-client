3a83828070bae244a18cb6db798a69b30035cc14
Dagon- <dagoncal@gmx.com>
2023-08-14 14:56:04 +0100

    Pull parameters and update table

--- psclient.py
+++ psclient.py
@@ -1,3 +1,5 @@
+import argparse
+import boto3
 from textual.app import App, ComposeResult
 from textual.widgets import Input, DataTable
 from textual.widgets import Pretty, Static, Footer, Header
@@ -27,6 +29,43 @@ DATA = {
     ]
 }
 
+class Paramaters():
+
+    list = []
+
+    def get_resources_from(self, ssm_details):
+        results = ssm_details['Parameters']
+        resources = [result for result in results]
+        next_token = ssm_details.get('NextToken', None)
+        return resources, next_token
+    
+    def get_session(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--profile", help = "AWS cli profile to connect aws with.")
+        parser.add_argument("--region", help = "Set AWS region.")
+        args = parser.parse_args()
+    
+        if args.profile:
+            session = boto3.Session(profile_name = args.profile)
+        else:    
+            session = boto3.Session()
+
+        client = session.client('ssm')
+    
+        return client
+    
+    def refresh(self):
+        client = self.get_session()
+        next_token = ' '
+
+        while next_token is not None:
+            ssm_details = client.describe_parameters(MaxResults = 50, NextToken = next_token)
+            current_batch, next_token = self.get_resources_from(ssm_details)
+            self.list += current_batch
+        
+    def __init__(self):
+        self.parameters = self.refresh()
+
 class Search(Static):
     
     def compose(self) -> ComposeResult:
@@ -41,13 +80,11 @@ class Results(Static):
     def compose(self) -> ComposeResult:
             yield Pretty(DATA)
 
-
 class psSearch(App):
 
     CSS_PATH = "pyclient.css"
 
     def compose(self) -> ComposeResult:
-        #yield Header()
         with Horizontal():
             yield Search(classes="column")
             yield Results(classes="column")
@@ -55,11 +92,21 @@ class psSearch(App):
 
 
     def on_mount(self) -> None:
-        table = self.query_one(DataTable)
-        table.add_columns(*ROWS[0])
-        table.add_rows(ROWS[1:])
+    # On start up we set up the boto3 session 
+    # and call a table refresh. This will load
+    # the table with all available parameters    
 
+        parameter = Paramaters()
+
+        table = self.query_one(DataTable)
+        table.add_columns("Parameter name", "Description")
+
+        for parameter in parameter.list:
+            if 'Description' not in parameter:
+                parameter['Description'] = ""
+            table.add_row(parameter['Name'], parameter['Description'])        
 
 if __name__ == "__main__":
+
     app = psSearch()
     app.run()
